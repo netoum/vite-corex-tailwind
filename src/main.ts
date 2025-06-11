a1546fe59dc97c57e76b2ff7025319181c574318
karimsemmoud <karim.semmoud@netoum.com>
2025-06-11 22:48:58 +0200

    0.0.4

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
 import './style.css'
 
 import "@netoum/corex";
+
+// import { initializeSiteSearch } from "@netoum/corex/components/site-search";
+// (async () => {
+//     try {
+//         // @ts-ignore
+//         const pagefind = await import("../dist/pagefind/pagefind.js");
+//         await pagefind.options({
+//             bundlePath: "../dist/pagefind/pagefind.js",
+//             baseUrl: "/",
+//         });
+//         await pagefind.init();
+//         console.log("Pagefind initialized");
+//         initializeSiteSearch(pagefind);
+//     } catch (error) {
+//         console.error("Failed to initialize Pagefind:", error);
+//     }
+// })();
